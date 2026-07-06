77472e46c6f1c88afebf29e17ec973252a864c06
Vez'NanDarkLord <941435991@qq.com>
2026-07-06 16:51:56 +0800

    Project Initialized!

---
+++ src/main/java/csulzc/medical_big_data_cloud/MedicalBigDataCloudApplication.java
@@ -0,0 +1,13 @@
+package csulzc.medical_big_data_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalBigDataCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalBigDataCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/csulzc/medical_big_data_cloud/MedicalBigDataCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package csulzc.medical_big_data_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalBigDataCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
